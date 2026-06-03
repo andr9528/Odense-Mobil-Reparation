d92e5ea423feadfb50d29a6f2da8e64dbf64d376
André Steenhoff Madsen <andre@steenhoff.dk>
2026-06-03 11:22:11 +0200

    feature: adds support for mouse back button clicked

--- Repair.Frontend/MainPage.cs
+++ Repair.Frontend/MainPage.cs
@@ -1,3 +1,5 @@
+using Microsoft.UI.Input;
+using Repair.Frontend.Abstraction;
 using Repair.Frontend.Extensions;
 using Repair.Frontend.Presentation;
 using Repair.Frontend.Presentation.Factory;
@@ -6,14 +8,45 @@ namespace Repair.Frontend;
 
 public sealed partial class MainPage : Page
 {
+    private readonly ILogger<MainPage> logger;
+
     public MainPage()
     {
+        logger = App.Startup.ServiceProvider.GetRequiredService<ILogger<MainPage>>();
+
         Background = new SolidColorBrush(Colors.LightGray);
 
         Grid contentGrid = CreateGrid();
         AddElementsToGrid(contentGrid);
 
+        PointerPressed += PointerPressedHandler;
+
         Content = contentGrid;
+    }
+
+    private void PointerPressedHandler(object sender, PointerRoutedEventArgs e)
+    {
+        try
+        {
+            PointerPointProperties properties = e.GetCurrentPoint(this).Properties;
+
+            if (!properties.IsXButton1Pressed)
+            {
+                return;
+            }
+
+            var navigationService = App.Startup.ServiceProvider.GetRequiredService<INavigationService>();
+
+            logger.LogDebug($"Back mouse button clicked - Attempting to Navigate Back.");
+            navigationService.NavigateBack();
+
+            e.Handled = true;
+        }
+        catch (Exception exe)
+        {
+            logger.LogError(exe, $"Exception caught during handling of Mouse click event.");
+            throw;
+        }
     }
 
     private void AddElementsToGrid(Grid contentGrid)
